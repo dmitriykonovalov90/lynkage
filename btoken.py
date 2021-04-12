95f80576393f4b2db26ac5129a0011d16581cfb3
dmitriykonovalov90 <martovskiy07kot@mail.ru>
2021-04-12 21:39:01 +0300

    Ковырял долго, но один хрен ничего не работает как надо((

--- btoken.py
+++ btoken.py
@@ -4,14 +4,14 @@ import json
 
 def get_token():
     body = {
-        "username": "o.filippova",
-        "password": "KkpGD4Yg",
+        "username": "ta.yakobson1",
+        "password": "iWAQtZMw",
         "client_secret": "23IzWSgkX5MUlpxSAYJr2o1sM8DRkLXI7vlZFExW",
         "grant_type": "password",
         "client_id": 2,
-        "organization": "rtk"
+        "organization": "lcentrix"
            }
-    response = requests.post('https://api.test.navigator.lynkage.ru/oauth/token', data=body)
+    response = requests.post('http://localhost/login', data=body)
     assert response.status_code != 500, "Необработанная ошибка от Laravel!"
     requestdict = json.loads(response.content)
     headers = {"Authorization": 'Bearer ' + requestdict['access_token']}
--- db_naumen.py
+++ db_naumen.py
@@ -3,10 +3,10 @@ import psycopg2
 
 def start_db_naumen():
     connection = psycopg2.connect(
-        database="rostel",
+        database="etl",
         user="dceo",
         password="dceo",
-        host='192.168.77.151',
+        host='192.168.77.152',
         port=5432,
     )
     return connection
